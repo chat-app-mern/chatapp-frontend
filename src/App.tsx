63576f90e219668cfd0eb033521bf089796f199e
rohilshah14 <rohil@krishaweb.com>
2025-03-02 19:35:05 +0530

    login-register page

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { apiClient } from "./utils/api.ts";
 import { QueryClientProvider } from "@tanstack/react-query";
 import Login from "./auth/Login.tsx";
+import Register from "./auth/Register.tsx";
 
 const router = createBrowserRouter([
-  { path: "/register", element: "" },
+  { path: "/register", element: <Register /> },
   { path: "/login", element: <Login /> },
   { path: "/forgot-password", element: "" },
   { path: "/verify-otp", element: "" },

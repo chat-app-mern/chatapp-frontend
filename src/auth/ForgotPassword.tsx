1bdbed73d92892e96d34f67df274ba49a5232eb0
maurya2201 <maurya@krishaweb.com>
2025-03-06 15:14:38 +0530

    Made auth common component and added in auth pages.

--- src/auth/ForgotPassword.tsx
+++ src/auth/ForgotPassword.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import * as yup from "yup";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { ReactComponent as Logo } from "../images/logo.svg";
 import ForgotPasswordImage from "../images/forgot-password-img.png";
 import { Link } from "react-router-dom";
+import AuthSection from "../components/AuthSection.tsx";
 
 const validationSchema = yup.object().shape({
   email: yup
@@ -29,57 +29,40 @@ const ForgotPassword: React.FC = () => {
   };
 
   return (
-    <section className="auth-form-wrapper">
-      <div className="container">
-        <div className="auth-logo">
-        <Logo width={200} height={50} />
+    <AuthSection
+      heading="Forgot your password?"
+      image={
+        <img
+          height="816"
+          width="616"
+          alt="forgot-password-image"
+          src={ForgotPasswordImage}
+        />
+      }
+      description="Don’t worry, happens to all of us. Enter your email below to recover your password"
+    >
+      <form onSubmit={handleSubmit(handleFormSubmit)}>
+        <div className="form-floating">
+          <input
+            type="text"
+            className="form-control"
+            id="floatingInput"
+            placeholder="123456"
+            {...register("email")}
+          />
+          <label htmlFor="floatingInput">Email</label>
+          {errors.email && (
+            <p className="auth-desc mt-2 text-danger">{errors.email.message}</p>
+          )}
         </div>
-        <div className="row">
-          <div className="col-lg-6">
-            <div className="auth-form">
-              <h1 className="auth-heading">Forgot your password?</h1>
-              <p className="auth-desc">
-                Don’t worry, happens to all of us. Enter your email below to
-                recover your password
-              </p>
-              <form onSubmit={handleSubmit(handleFormSubmit)}>
-                <div className="form-floating">
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="floatingInput"
-                    placeholder="123456"
-                    {...register("email")}
-                  />
-                  <label htmlFor="floatingInput">Email</label>
-                  {errors.email && (
-                    <p className="auth-desc mt-2 text-danger">
-                      {errors.email.message}
-                    </p>
-                  )}
-                </div>
-                <button type="submit" className="btn btn-primary w-100 mt-4">
-                  Submit
-                </button>
-              </form>
-              <Link className="auth-desc text-center" to="/login">
-                <span>Back to Login</span>
-              </Link>
-            </div>
-          </div>
-          <div className="col-lg-6">
-            <div className="photo-wrapper">
-              <img
-                height="816"
-                width="616"
-                alt="forgot-password-image"
-                src={ForgotPasswordImage}
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+        <button type="submit" className="btn btn-primary w-100 mt-4">
+          Submit
+        </button>
+      </form>
+      <Link className="auth-desc text-center" to="/login">
+        <span>Back to Login</span>
+      </Link>
+    </AuthSection>
   );
 };
 
